4c9fa072ed6f599ef5075d8facf809923f8659cb
bboysquiz <mrkiraller@gmail.com>
2023-10-03 14:27:50 +0300

    created store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import { api } from "./api";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer
+  },
+});
 
 function App() {
   return (
+    <Provider store={store}>
       <Router>
         <Routes />
       </Router>
+    </Provider>
   );
 }
 
