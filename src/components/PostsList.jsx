6eb06d6b3e2e2b68218ed6e25567ae4b615d6a00
bboysquiz <mrkiraller@gmail.com>
2023-10-03 14:06:04 +0300

    created structure of PostsList

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const PostsList = () => {
+    const post = {
+        id: 0,
+        title: 'title',
+        body: 'body',
+    }
   return (
-    <div>PostsList</div>
+    <ul>
+        <li>
+            <div className="post-id">{post.id}</div>
+            <div className="post-title">{post.title}</div>
+            <div className="post-body">{post.body}</div>
+            <Link to={`/posts/${post.id}`}>Просмотр</Link>
+        </li>
+    </ul>
   )
 }
 
